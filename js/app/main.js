690b23e49a590397942700c6afb0995eb2204dd9
Emanuel Farauanu <rototuro@gmail.com>
2016-05-23 13:51:11 +0300

    shizuoSelect

--- js/app/main.js
+++ js/app/main.js
@@ -114,7 +114,7 @@ var AppModule = (function () {
 $(document).on("ready", function () {
    AppModule.init();
    AppModule.bindHandlers();
-   AppModule.loaded();
+   $(window).load(AppModule.loaded());
 });
 
 window.displayLoadingScreen = function() {
